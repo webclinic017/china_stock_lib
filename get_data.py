dbe475fbe6b7dea4ec2dc4c50ddb987afb8a0f4c
Alex J.Y <33644601+jinyiabc@users.noreply.github.com>
2021-12-25 14:45:11 +0800

    update

--- get_data.py
+++ get_data.py
@@ -1,19 +1,15 @@
 import os
 import zipfile
-import urllib.request 
+import urllib.request
 from urllib.error import HTTPError
 
 base_url = "https://github.com/jinyiabc/china_stock_data/raw/main/"
 
+
 def get_data(rel_url):
     url = base_url + rel_url
     local_file = os.path.basename(url)
-    print(local_file)
-    print(url)
     try:
-        # opener = urllib.request.build_opener()
-        # opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-        # urllib.request.install_opener(opener)
         urllib.request.urlretrieve(url, local_file)
     except HTTPError as err:
         print(err)
@@ -32,9 +28,6 @@ def get_data(rel_url):
 def get_module_1(username):
     get_data("module-{0:02n}/{1:s}.zip".format(1, username))
 
+
 def get_module_2(username):
     get_data("module-{0:02n}/{1:s}.zip".format(2, username))
-
-
-if __name__ == '__main__':
-    get_module_1('survivorship_free')

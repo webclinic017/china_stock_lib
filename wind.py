cfcdf9d2c04e186a0e79d6c6a680ed32b6f80457
jinyi <jinyiabc@gmail.com>
2021-12-26 17:30:16 +0800

    trivial

--- wind.py
+++ wind.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 import pandas as pd
 import backtrader as bt
-
+from backtrader import TimeFrame
+from backtrader.utils.dateintern import date2num
 
 def read_wind_excel(ticker, start, end, skiprows, header):
     custom_col_converter = lambda x: float(x.replace('--', '0'))
@@ -47,3 +50,78 @@ class PandasDataFeed(bt.feeds.PandasDirectData):
         ('volume', 9),
         ('openinterest', -1),
     )
+
+
+class CSVDataFeed(bt.feeds.GenericCSVData):
+    params = (
+        ('dtformat', '%Y-%m-%d'),
+        ('datetime', 0),
+        ('time', -1),
+        ('open', 1),
+        ('high', 2),
+        ('low', 3),
+        ('close', 4),
+        ('volume', 5),
+        ('openinterest', -1),
+    )
+
+    def _loadline(self, linetokens):
+        # Datetime needs special treatment
+        dtfield = linetokens[self.p.datetime]
+        if self._dtstr:
+            dtformat = self.p.dtformat
+
+            if self.p.time >= 0:
+                # add time value and format if it's in a separate field
+                dtfield += 'T' + linetokens[self.p.time]
+                dtformat += 'T' + self.p.tmformat
+
+            dt = datetime.strptime(dtfield, dtformat)
+        else:
+            dt = self._dtconvert(dtfield)
+
+        if self.p.timeframe >= TimeFrame.Days:
+            # check if the expected end of session is larger than parsed
+            if self._tzinput:
+                dtin = self._tzinput.localize(dt)  # pytz compatible-ized
+            else:
+                dtin = dt
+
+            dtnum = date2num(dtin)  # utc'ize
+
+            dteos = datetime.combine(dt.date(), self.p.sessionend)
+            dteosnum = self.date2num(dteos)  # utc'ize
+
+            if dteosnum > dtnum:
+                self.lines.datetime[0] = dteosnum
+            else:
+                # Avoid reconversion if already converted dtin == dt
+                self.l.datetime[0] = date2num(dt) if self._tzinput else dtnum
+        else:
+            self.lines.datetime[0] = date2num(dt)
+
+        # The rest of the fields can be done with the same procedure
+        for linefield in (x for x in self.getlinealiases() if x != 'datetime'):
+            # Get the index created from the passed params
+            csvidx = getattr(self.params, linefield)
+
+            if csvidx is None or csvidx < 0:
+                # the field will not be present, assignt the "nullvalue"
+                csvfield = self.p.nullvalue
+            else:
+                # get it from the token
+                csvfield = linetokens[csvidx]
+
+            if csvfield == '':
+                # if empty ... assign the "nullvalue"
+                csvfield = self.p.nullvalue
+
+            if csvfield == '--':
+                # if empty ... assign the "nullvalue"
+                csvfield = 0
+
+            # get the corresponding line reference and set the value
+            line = getattr(self.lines, linefield)
+            line[0] = float(float(csvfield))
+
+        return True

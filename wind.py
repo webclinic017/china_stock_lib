e1a6f22946d7f34045ca873495b1619b49d55f23
Alex J.Y <33644601+jinyiabc@users.noreply.github.com>
2021-12-30 03:05:08 +0800

    trivial

--- wind.py
+++ wind.py
@@ -32,9 +32,38 @@ def read_wind_excel(ticker, start, end, skiprows, header):
             "成交量(万股)": "volume",
         },
     )
-    df0.rename_axis('date', inplace=True)
     df0.sort_index(inplace=True)
-    df0[start:end]
+
+    return df0[start:end]
+
+def read_wind_csv(ticker, start, end, skiprows, header):
+    custom_col_converter = lambda x: float(x.replace('--', '0'))
+    df0 = pd.read_csv(ticker,
+                        encoding='GB2312',
+                        parse_dates=['日期'],
+                        thousands=',',
+                        converters={'开盘点位': custom_col_converter,
+                                    "最高点位": custom_col_converter,
+                                    "最低点位": custom_col_converter,
+                                    "收盘价": custom_col_converter,
+                                    "成交量(万股)": custom_col_converter,
+                                    },
+                        index_col=0,
+                        skiprows=skiprows,
+                        header=header,
+                        ) \
+        .rename(
+        columns={
+            "日期": "datetime",
+            "开盘点位": "open",
+            "最高点位": "high",
+            "最低点位": "low",
+            "收盘价": "close",
+            "成交量(万股)": "volume",
+        },
+    )
+    df0.set_index('datetime', inplace=True)
+    df0.sort_index(inplace=True)
 
     return df0[start:end]
 

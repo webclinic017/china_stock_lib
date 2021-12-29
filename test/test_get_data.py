d3777e720a1fd8c9a2411901bd66da786c0ed241
Alex J.Y <33644601+jinyiabc@users.noreply.github.com>
2021-12-30 03:05:08 +0800

    trivial

--- test/test_get_data.py
+++ test/test_get_data.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 import get_data
-from wind import read_wind_excel
+from wind import read_wind_excel, read_wind_csv
 
 
 def test_get_data():
@@ -13,5 +13,16 @@ def test_get_data():
 
 def test_read_wind_excel():
     dir = os.path.join('..', '../quant/_notebooks')
-    df = read_wind_excel(f'{dir}/csi300.xlsx', '2015-6-18', '2018-6-18')
+    df = read_wind_excel(f'{dir}/csi300.xlsx', '2015-6-18', '2018-6-18',
+                                 skiprows=0,   # false
+                                 header=0,)    # start from 1st line
+    print(df)
+    pass
+
+def test_read_wind_csv():
+    dir = os.path.join('..', '../quant/CSI/wind_csi300')
+    df = read_wind_csv(f'{dir}/600602.SH.CSV', '2015-6-18', '2018-6-18',
+                                skiprows=0,
+                                 header=0,)
+    print(df)
     pass

6745c0db489f2e35af5f9f90f06e1743d0525cd1
Alex J.Y <33644601+jinyiabc@users.noreply.github.com>
2022-01-22 15:39:35 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'stock_data',
-    version = '0.0.9',
+    version = '0.0.10',
     author = 'Alex JIN YI',
     author_email = 'jinyiabc@gmail.com',
     packages=['helper'],
